449a536d73e0fbf41d8d725616f2a1e7b7df6434
JFelc <julien.felici@gmail.com>
2022-01-03 15:04:29 +0100

    Fix branch 2 (To prevent merge problems)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,25 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Figure;
-use App\Entity\Photo;
 
-class HomeController extends AbstractController
-{  
-     /**
-     * @Route("/home", name="home")
+class FigureController extends AbstractController
+{
+    /**
+     * @Route("/figure", name="figure")
      */
-    public function displayHome(): Response
+    public function index(): Response
     {
-        $figure = new Figure;
-        $name = $figure->getName();
-        $photo = $figure->getPhotos();
-
-        $newPhoto = new Photo;
-        $nPhoto = $newPhoto->getName();
-         
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController','photo'=>$photo,'name'=>$name
+        return $this->render('figure/index.html.twig', [
+            'controller_name' => 'FigureController',
         ]);
     }
 }
