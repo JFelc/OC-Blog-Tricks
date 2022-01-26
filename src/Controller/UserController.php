6b7a4ad6012542399d89d5a20651305395845408
JFelc <julien.felici@gmail.com>
2022-01-26 19:01:24 +0100

    Login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,16 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
+  /**
+     * @Route("/login", name="security_login")
      */
-    public function index(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        // retrouver une erreur d'authentification s'il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // retrouver le dernier identifiant de connexion utilisé
+        $lastUsername = $authenticationUtils->getLastUsername();
+        
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            ]
+        );
     }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+
+    
 }
